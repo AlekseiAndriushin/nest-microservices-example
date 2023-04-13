b249c99e3f24b75e5b7031214762e0a4d42eb0ff
Alexey Andryushin <andr200116alex@gmail.com>
2023-04-13 12:13:29 +0300

    add base crud & logging & validating

--- apps/earler/src/app.module.ts
+++ apps/earler/src/app.module.ts
@@ -2,9 +2,10 @@ import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
